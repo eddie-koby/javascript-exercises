b19beb3aa61ffc14ff0439192bccad8790839838
Eddie Koby <ekoby7@gmail.com>
2022-09-03 14:32:08 -0700

    Complete javascript practice exercises 01 - 07
    Completed exercises to practice basic javascript

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let repeatedStr = '';
 
+    if(num < 0) return 'ERROR';
+
+    for(let i =0; i < num; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let cTemp = (temp - 32) * 5 / 9;
+  cTemp = Math.round(cTemp * 10) / 10;
+  return cTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fTemp = (temp * 9 / 5) + 32;
+  fTemp = Math.round(fTemp * 10) / 10;
+  return fTemp;
 };
 
 // Do not edit below this line
